33b0de586f9596570e494b397505a4fa0503ed7c
Mohammed alJaser <aljaserm7@gmail.com>
2019-03-25 19:48:27 -0400

    Added the files

---
+++ NumberConversion.py
@@ -0,0 +1,7 @@
+# conversion of numbers
+
+num=int(input("Enter a Number: "))
+
+print("Binary Conversion Value is :{}".format(bin(num)))
+print("Octal Conversion Value is :{}".format(oct(num)))
+print("Hexa Conversion Value is :{}".format(hex(num)))
